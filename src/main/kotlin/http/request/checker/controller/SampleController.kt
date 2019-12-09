7b5ba8245892c4b83349d774a31625ff6bd5dd99
Takafumi Iju <ijufumi@gmail.com>
2019-12-09 21:40:59 +0900

    Modify logging

--- src/main/kotlin/http/request/checker/controller/SampleController.kt
+++ src/main/kotlin/http/request/checker/controller/SampleController.kt
@@ -1,12 +1,11 @@
 package http.request.checker.controller
 
-import io.micronaut.http.HttpRequest
 import io.micronaut.http.MediaType
+import io.micronaut.http.annotation.Body
 import io.micronaut.http.annotation.Controller
 import io.micronaut.http.annotation.Get
 import io.micronaut.http.annotation.Post
 import org.slf4j.LoggerFactory
-import java.awt.PageAttributes
 
 
 @Controller("/")
@@ -14,8 +13,8 @@ class SampleController {
     val logger = LoggerFactory.getLogger("test")
 
     @Post(consumes = [MediaType.APPLICATION_JSON])
-    fun post(request: HttpRequest<Any>): String {
-        logger.info("request body:{}", request.body)
+    fun post( @Body payload: String): String {
+        logger.info("request body:{}", payload)
         return "hello, world"
     }
 
