23d41f83189036866af10b2c8bebbdddd0ebb567
Takafumi Iju <ijufumi@gmail.com>
2019-12-10 14:12:04 +0900

    Add checks of box message.

--- src/main/kotlin/http/request/checker/controller/SampleController.kt
+++ src/main/kotlin/http/request/checker/controller/SampleController.kt
@@ -7,19 +7,21 @@ import io.micronaut.http.annotation.Controller
 import io.micronaut.http.annotation.Get
 import io.micronaut.http.annotation.Post
 import org.slf4j.LoggerFactory
+import com.box.sdk.BoxWebHookSignatureVerifier
 
 
 @Controller("/")
 class SampleController {
     val logger = LoggerFactory.getLogger("test")
-
+    val primaryKey = System.getenv("BOX_PRIMARY_KEY")
+    val secondaryKey = System.getenv("BOX_SECONDARY_KEY")
     @Post(consumes = [MediaType.APPLICATION_JSON])
     fun post(
             @Header("box-delivery-id") id: String,
             @Header("box-delivery-timestamp") timestamp: String,
             @Header("box-signature-algorithm") algorithm: String,
-            @Header("box-signature-primary") primaryKey: String,
-            @Header("box-signature-secondary") secondaryKey: String,
+            @Header("box-signature-primary") primarySignature: String,
+            @Header("box-signature-secondary") secondarySignature: String,
             @Header("box-signature-version") version: String,
             @Body body: String
     ): String {
@@ -27,12 +29,15 @@ class SampleController {
         logger.info("[box-delivery-id]:{}", id)
         logger.info("[box-delivery-timestamp]:{}", timestamp)
         logger.info("[box-signature-algorithm]:{}", algorithm)
-        logger.info("[box-signature-primary]:{}", primaryKey)
-        logger.info("[box-signature-secondary]:{}", secondaryKey)
+        logger.info("[box-signature-primary]:{}", primarySignature)
+        logger.info("[box-signature-secondary]:{}", secondarySignature)
         logger.info("[box-signature-version]:{}", version)
         logger.info("request headers -----")
 
         logger.info("request body:{}", body)
+
+        val validation = BoxWebHookSignatureVerifier(primaryKey, secondaryKey)
+        logger.info("validation:{}", validation.verify(version, algorithm, primarySignature, secondarySignature, body, timestamp))
         return "hello, world"
     }
 
