9ec40603bc21009efb07d5ba4ac6ca36025cc647
Takafumi Iju <ijufumi@gmail.com>
2019-12-10 13:36:45 +0900

    Add logging of request headers.

--- src/main/kotlin/http/request/checker/controller/SampleController.kt
+++ src/main/kotlin/http/request/checker/controller/SampleController.kt
@@ -2,6 +2,7 @@ package http.request.checker.controller
 
 import io.micronaut.http.MediaType
 import io.micronaut.http.annotation.Body
+import io.micronaut.http.annotation.Header
 import io.micronaut.http.annotation.Controller
 import io.micronaut.http.annotation.Get
 import io.micronaut.http.annotation.Post
@@ -13,8 +14,25 @@ class SampleController {
     val logger = LoggerFactory.getLogger("test")
 
     @Post(consumes = [MediaType.APPLICATION_JSON])
-    fun post( @Body payload: String): String {
-        logger.info("request body:{}", payload)
+    fun post(
+            @Header("box-delivery-id") id: String,
+            @Header("box-delivery-timestamp") timestamp: String,
+            @Header("box-signature-algorithm") algorithm: String,
+            @Header("box-signature-primary") primaryKey: String,
+            @Header("box-signature-secondary") secondaryKey: String,
+            @Header("box-signature-version") version: String,
+            @Body body: String
+    ): String {
+        logger.info("request headers -----")
+        logger.info("[box-delivery-id]:{}", id)
+        logger.info("[box-delivery-timestamp]:{}", timestamp)
+        logger.info("[box-signature-algorithm]:{}", algorithm)
+        logger.info("[box-signature-primary]:{}", primaryKey)
+        logger.info("[box-signature-secondary]:{}", secondaryKey)
+        logger.info("[box-signature-version]:{}", version)
+        logger.info("request headers -----")
+
+        logger.info("request body:{}", body)
         return "hello, world"
     }
 
